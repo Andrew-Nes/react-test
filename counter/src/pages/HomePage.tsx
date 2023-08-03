ce842a77bf428f2f17ca794985e4bb93dde04ee9
Andrei Niasmachny <Cejosoll@gmail.com>
2023-08-03 15:43:19 +0300

    feat: add counter

--- counter/src/pages/HomePage.tsx
+++ counter/src/pages/HomePage.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import DateDisplay from '../components/DateDisplay'
-
+import Counter from '../components/counter/counter'
 const HomePage: React.FC = () => {
     return (
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <h1 style={{ fontSize: '4em' }}>Click buttons to count</h1>
-            <DateDisplay />
+            <Counter></Counter>
         </div>
     )
 }
